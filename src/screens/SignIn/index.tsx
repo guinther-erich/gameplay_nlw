f1977f1c4169b3fa04dc43cbc3c7c6a0091f5dcb
Guinther Erich K S Anders <67793128+guinther-erich@users.noreply.github.com>
2021-06-22 19:45:29 -0300

    Maximum Speed

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 import { 
   View, 
   Text, 
-  Image,
-  StatusBar } from 'react-native';
+  Image, 
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
+import { ButtonIcon } from '../../components/ButtonIcon';
+
 export function SignIn() {
+  const navigation = useNavigation();
+  function handleSignIn() {
+    navigation.navigate('Home');
+  }
 
   return (
     <View style={ styles.container }>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor= "transparent"
-        translucent/>
       <Image 
       source={IllustrationImg}
       style={styles.image} 
@@ -25,9 +27,9 @@ export function SignIn() {
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {'\n'}
-          suas jogatinas {'\n'}
-          facilmente
+          Conecte-se {'\n'}
+          e organize suas {'\n'}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
@@ -37,9 +39,8 @@ export function SignIn() {
 
         <ButtonIcon 
         title="Entrar com Discord"
-        activeOpacity={0.7}
+        onPress={handleSignIn}
         />
-
       </View>
     </View>
   );
